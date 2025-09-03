c172ccd79a2e680d00c83efde8ef9ab8b5e82634
Giancarlo Rosso <g.rosso@fineco.it>
2025-09-03 16:09:33 +0200

    fix: [curl] rimosso token

--- scripts/pipelint_curl.sh
+++ scripts/pipelint_curl.sh
@@ -2,6 +2,6 @@
 
 set -x
 JENKINS_URL=https://cvs.fineco.it/jenkins
-#JENKINS_CRUMB=$(curl --user jenkins-user:Cvbhui135 "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
-#curl --user jenkins-user:Cvbhui135 -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<$1" $JENKINS_URL/pipeline-model-converter/validate
-curl --user giancarlorosso:11e9b9eb489d43be2bf69727a1f605b34d -X POST -F "jenkinsfile=<$1" ${JENKINS_URL}/pipeline-model-converter/validate
+#JENKINS_CRUMB=$(curl --user jenkins-user: "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
+#curl --user jenkins-user: -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<$1" $JENKINS_URL/pipeline-model-converter/validate
+curl --user giancarlorosso: -X POST -F "jenkinsfile=<$1" ${JENKINS_URL}/pipeline-model-converter/validate
